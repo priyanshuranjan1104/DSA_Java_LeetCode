45b4134ad8e4ca4fd7d19bca939b615eb29cc9d7
Priyanshu Ranjan <priyanshuranjan000004@gmail.com>
2026-04-22 21:03:57 +0530

    Time: 14 ms (94.08%) | Memory: 51.7 MB (22.11%) - LeetSync

--- 455-assign-cookies/assign-cookies.java
+++ 455-assign-cookies/assign-cookies.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 class Solution {
     public int findContentChildren(int[] g, int[] s) {
         Arrays.sort(g);
@@ -6,7 +8,7 @@ class Solution {
         int i = 0, j = 0;
 
         while (i < g.length && j < s.length) {
-            if (s[j] >= g[i]) i++;
+            if (g[i] <= s[j]) i++;
             j++;
         }
 
